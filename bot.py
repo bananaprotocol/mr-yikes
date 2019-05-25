55d5192632a08c7e25dff48bcfab7f0bc35fe5da
bananaprotocol <bananaprotocol@protonmail.com>
2019-05-25 03:33:21 +0200

    Add role support

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.utils import get
 
 TOKEN=os.environ['TOKEN']
 TARGET_ID=os.environ['TARGET_ID']
+TARGET_ROLE=os.environ['TARGET_ROLE']
 EMOTE=os.environ['EMOTE']
 
 class MyClient(discord.Client):
@@ -11,11 +12,15 @@ class MyClient(discord.Client):
         print('Logged in as', self.user)
 
     async def on_message(self, message):
+        emote = get(message.guild.emojis, name=EMOTE)
+
         if message.author == self.user:
             return
 
         if message.author.id == int(TARGET_ID):
-            emote = get(message.guild.emojis, name=EMOTE)
+            await message.add_reaction(emote)
+
+        if TARGET_ROLE.lower() in [i.name.lower() for i in message.author.roles]:
             await message.add_reaction(emote)
 
 client = MyClient()
